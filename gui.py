d25123baa7947765d62c108a0a711154de9e0fe9
skawy <elskawyahmedfci>
2022-09-14 00:14:08 +0200

    finishing the project by adding models to gui file and done predicting

--- gui.py
+++ gui.py
@@ -1,85 +1,113 @@
-from fileinput import filename
 import PySimpleGUI as sg
+import os
 import os.path
 import sys
 import pandas as pd
+from PIL import Image
 
-from models.models import CNN, CustomModel
+from models.models import  CustomModel, MobileNet, CNN
 sys.path.append("/home/skawy/side_projects/Sports-Image-Classification/models")
 
 
-df = pd.read_csv('/home/skawy/side_projects/Sports-Image-Classification/class_dict.csv')
-sports = df['class'] 
-
-file_list_column = [
-    [
-        sg.Text("Image Folder"),
-        sg.In(size=(40,1) , enable_events=True, key="-FOLDER-"),
-        sg.FolderBrowse(),
-    ],
-    [sg.HSeparator(pad = (0,40))],
-    [
-        sg.Listbox(
-            values=[], enable_events=True , size=(50,60),
-            key= "-FILE LIST-"
-        )
-    ],
-    
-]
+def jpg_to_png(filename):
+    im1 = Image.open(filename)
+    png_file  = filename.replace('.jpg','.png')
+    im1.save(png_file)
+    return png_file
 
 
-image_viewer_column = [
-    [sg.Text("Choose an Image From The list on the Left")],
-    [sg.Text(size=(40,1) , key= "-TOUT-")],
-    [sg.Image(key="-IMAGE-" )],
-    [sg.Text("This Sport is: " , key= "-SPORT NAME-")]
-]
+def main():
+    model1 = CustomModel(CNN())
+    model2 = CustomModel(MobileNet())
 
-layout = [
-    [
-        sg.Column(file_list_column),
-        sg.VSeparator(),
-        sg.Column(image_viewer_column),
-    ]
-]
+    sports_df = pd.read_csv('/home/skawy/side_projects/Sports-Image-Classification/class_dict.csv')
+    labels = sports_df['class'] 
 
-window = sg.Window("Image Viewer", layout , resizable= True).finalize()
-window.maximize()
 
-# event loop
-
-while True:
-    event,values = window.read()
-    if event == "Exit" or event == sg.WIN_CLOSED:
-        break
-
-    if event == "-FOLDER-":
-        folder = values["-FOLDER-"]
-        try:
-            file_list = os.listdir(folder)  # type: ignore
-        except:
-            file_list = []
-        
-        fnames= [
-            f 
-            for f in file_list
-            if os.path.isfile(os.path.join(folder,f))
-            and f.lower().endswith((".png",".gif"))
-        ]
-        window["-FILE LIST-"].update(fnames)
-    elif event == "-FILE LIST-":
-        try:
-            filename = os.path.join(
-                values["-FOLDER-"], values["-FILE LIST-"] [0]
+    file_list_column = [
+        [
+            sg.Text("Image Folder"),
+            sg.In(size=(40,1) , enable_events=True, key="-FOLDER-"),
+            sg.FolderBrowse(),
+        ],
+        [sg.HSeparator(pad = (0,40))],
+        [
+            sg.Listbox(
+                values=[], enable_events=True , size=(50,60),
+                key= "-FILE LIST-"
             )
-            window["-TOUT-"].update(filename)
-            window["-IMAGE-"].update(filename = filename, size = (720,720))
-            Cnn = CustomModel(CNN())
-    
-            window["-SPORT NAME-"].update(filename)
-        except:
-            pass
+        ],
+        
+    ]
 
 
-window.close()
+    image_viewer_column = [
+        [sg.Text("Choose an Image From The list on the Left")],
+        [sg.Text(size=(100,2) , key= "-filename-")],
+        [sg.Image(key="-IMAGE-" )],
+        [sg.Text("" , key= "-MODEL1 SPORT NAME-")],
+        [sg.Text("" , key= "-MODEL2 SPORT NAME-")]
 
+    ]
+
+
+    layout = [
+        [
+            sg.Column(file_list_column),
+            sg.VSeparator(),
+            sg.Column(image_viewer_column),
+        ]
+    ]
+
+    font = ("Arial", 12)
+    output_font = ("Cooper Black" , 18)
+    window = sg.Window("Image Viewer", layout ,font=font , resizable= True).finalize()
+    window.maximize()
+
+    # event loop
+
+    while True:
+
+        event,values = window.read()  # type: ignore
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+
+        if event == "-FOLDER-":
+            folder = values["-FOLDER-"]
+            try:
+                file_list = os.listdir(folder)  # type: ignore
+            except:
+                file_list = []
+            
+            files= [
+                file 
+                for file in file_list
+                if os.path.isfile(os.path.join(folder,file))
+                and file.lower().endswith((".png" , ".jpg",".gif"))
+            ]
+            window["-FILE LIST-"].update(files)
+
+        elif event == "-FILE LIST-":
+            try:
+                filename = os.path.join(
+                    values["-FOLDER-"], values["-FILE LIST-"] [0]
+                )
+                window["-filename-"].update(filename)
+
+                if filename.endswith('.jpg'):
+                    filename = jpg_to_png(filename)
+
+                window["-IMAGE-"].update(filename = filename, size = (720,720))
+                prediction1 = labels[model1.predict(filename)]
+                prediction2 = labels[model2.predict(filename)]
+                window["-MODEL1 SPORT NAME-"].update(f'CNN Predicting: {prediction1}' , font = output_font)
+                window["-MODEL2 SPORT NAME-"].update(f'MobileNet Predicting: {prediction2}' , font = output_font)
+            except:
+                pass
+
+
+    window.close()
+
+
+if __name__ == '__main__':
+    main()
--- models/models.py
+++ models/models.py
@@ -5,6 +5,11 @@ import cv2
 import matplotlib.pyplot as plt
 from tensorflow.keras import (Input, Model, layers, losses, optimizers, metrics, utils, models)
 # from os.path import exists as file_exists
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from keras.models import Sequential, Model
+from keras.layers import Conv2D, MaxPooling2D,GlobalAveragePooling2D
+from keras.layers import Activation, Dropout, BatchNormalization, Flatten, Dense, AvgPool2D,MaxPool2D
 
 class Strategy(ABC):
     model: Model 
@@ -39,7 +44,7 @@ class CNN(Strategy):
         batch_size = 64
         epochs = 1
         learning_rate = 1e-3 
-        weights_file = '/home/skawy/side_projects/Sports-Image-Classification/cnn_weights.h5'
+        weights_file = '/home/skawy/side_projects/Sports-Image-Classification/models/weights/cnn_weights.h5'
         self.model.compile(optimizer = optimizers.Adam(learning_rate),   # type: ignore
                         loss = losses.categorical_crossentropy, 
                         metrics = ['accuracy'])
@@ -47,6 +52,45 @@ class CNN(Strategy):
         self.model.load_weights(weights_file)  # type: ignore
 
         # history = model.fit(train_generator, validation_data= val_generator, epochs = EPOCHS)
+    
+    def  predict(self,image) -> float:
+        return self.model.predict(image)
+
+
+class MobileNet(Strategy):
+    def __init__(self) -> None:
+        base_model = MobileNetV2(include_top=False, input_shape=self.image_shape)
+        for layer in base_model.layers:
+            layer.trainable = False
+
+        model = Sequential()
+        model.add(base_model)
+        model.add(GlobalAveragePooling2D())
+        model.add(Dense(128))
+        model.add(Dropout(0.1))
+        model.add(Dense(128))
+        model.add(Dropout(0.1))
+        model.add(Dense(self.n_classes, activation = 'softmax'))
+
+        self.model = model
+        self.loading_weights()  # type: ignore
+        super().__init__()
+
+    def loading_weights(self) -> None:
+        batch_size = 64
+        epochs = 1
+        learning_rate = 1e-3 
+        weights_file = '/home/skawy/side_projects/Sports-Image-Classification/models/weights/mobilenet_weights1.h5'
+        self.model.compile(optimizer = optimizers.Adam(learning_rate),   # type: ignore
+                        loss = losses.categorical_crossentropy, 
+                        metrics = ['accuracy'])
+
+        self.model.load_weights(weights_file )  # type: ignore
+
+        # history = model.fit(train_generator, validation_data= val_generator, epochs = EPOCHS)
+    
+    def  predict(self,image) -> float:
+        return self.model.predict(image)
 
         
 class CustomModel:
